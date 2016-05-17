b66271e8b3820f47de3c63f50b160761c03059d8
tjgrst <tjgrst@gmail.com>
2016-05-17 13:47:54 -0500

    initial commit

---
+++ Sweepstakes/ICustomString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    interface ICustomString
+    {
+        string ToString();
+        void Insert(int startIndex, string stringToInsert);
+        void Remove(int startIndex, int numCharsToRemove);
+        int Length();
+    }
+}
---
+++ Sweepstakes/SystemString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class SystemString : ICustomString
+    {
+
+        public void Insert(int startIndex, string stringToInsert)
+        {
+
+        }
+
+        public void Remove(int startIndex, int numCharsToRemove)
+        {
+
+        }
+
+        public int Length()
+        {
+            return 100;
+        }
+    }
+}
